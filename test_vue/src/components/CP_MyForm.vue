2e85067c331c37435dffcaf6ac2d0a574a413850
Myzhibo <douzhibo111@163.com>
2023-09-20 14:31:24 +0800

    feat: 表单组件

---
+++ test_vue/src/components/CP_MyForm.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="myForm">
+      <span style="margin-right: 20px;font-size: 15px;">类型</span>  
+      <el-select style="width: 250px;" v-model="dynamicExpansion.type" placeholder="请选择">
+        <el-option key="PPT_AUTO_VBA_J2PPT" label="PPT_AUTO_VBA_J2PPT" value="PPT_AUTO_VBA_J2PPT"></el-option>
+        <el-option key="PPT_AUTO_VBA_W2PPT" label="PPT_AUTO_VBA_W2PPT" value="PPT_AUTO_VBA_W2PPT"></el-option>
+        <el-option key="CONTENT_SERVER_NODE" label="CONTENT_SERVER_NODE" value="CONTENT_SERVER_NODE"></el-option>
+      </el-select>
+      <br><br>
+      <span style="margin-right: 20px;font-size: 15px;">数量</span>  
+      <el-select style="width: 250px;" v-model="dynamicExpansion.num" placeholder="请选择">
+        <el-option v-for="(item,index) in 50"
+                    :key="index"
+                    :label="item"
+                    :value="item"
+                    >
+        </el-option>
+      </el-select>
+      <br><br>
+      <span style="margin-right: 20px;font-size: 15px;">时间</span>  
+        <el-input-number v-model="dynamicExpansion.time" style="width:250px;" @change="handleChange" :min="1" :max="3" label="描述文字"></el-input-number>
+        <span style="font-size: 13px; color:gray; position: relative; left: 10px; top:5px">小时</span>
+      <br><br><br>
+      <el-button @click="startExpand" size="mini" style="width:295px;margin-left: 5px;" type="primary" >开始扩容</el-button>
+      <br><br>
+      <!-- <img v-if="fileList[0].url" :src="fileList[0].url" style="width: 100px;height: 100px;" >
+      <i v-if="fileList[0].url" style="font-size: 16px;margin-left: 10px;cursor: pointer;" class="el-icon-delete" 
+              @click="deleteImg"></i>
+      <div v-if="fileList.length ===0 " style="margin-top: 10px;" >无封面</div>
+      <el-upload
+        
+      multiple 
+      class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :before-remove="beforeRemove"
+                :limit="3"
+                :on-exceed="handleExceed"
+                :file-list="fileList">
+                <el-button size="small" type="primary">点击上传</el-button>
+                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+              </el-upload>
+      <br><br> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        // 表单相关
+        dynamicExpansion:{
+          type:'PPT_AUTO_VBA_J2PPT',
+          time: '1',
+          num: '1'
+        },
+        // 上传相关
+        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+      
+      }
+    },
+    methods:{
+        // 计数器
+        handleChange(val){
+          console.log(val);
+        },
+        // 提交按钮
+        startExpand(){
+          this.$emit('getShow',false)
+          console.log(this.dynamicExpansion);
+        },
+        /** upload相关 */
+        handleRemove(file, fileList) {
+          console.log(file, fileList);
+        },
+        handlePreview(file) {
+          console.log(file);
+        },
+        handleExceed(files, fileList) {
+          this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+        },
+        beforeRemove(file, fileList) {
+          console.log(fileList);
+          return this.$confirm(`确定移除 ${ file.name }？`);
+        }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- test_vue/src/components/HelloWorld.vue
+++ test_vue/src/components/HelloWorld.vue
@@ -11,59 +11,16 @@
     table组件 + 假分页
     <CP_table_fakePaging :ListData="ListData" />
     <hr>
-    <!-- FUNCTION: 表单弹窗 -->
+    <!-- FUNCTION: 弹窗 -->
     表单弹窗组件
     <el-button  size="mini" type="primary"  @click="isShows=true">打开弹窗</el-button>
     <CP_MyDialog :isShows="isShows" title="添加机器" @getShow="getShow">
         <!-- 向子组件的插槽中插入 -->
         <div slot="option">
-                    <div style="display: flex ;align-items: center; justify-content: center;">
-                        <div>
-                            <span style="margin-right: 20px;font-size: 15px;">类型</span>  
-                            <el-select style="width: 250px;" v-model="dynamicExpansion.type" placeholder="请选择">
-                              <el-option key="PPT_AUTO_VBA_J2PPT" label="PPT_AUTO_VBA_J2PPT" value="PPT_AUTO_VBA_J2PPT"></el-option>
-                              <el-option key="PPT_AUTO_VBA_W2PPT" label="PPT_AUTO_VBA_W2PPT" value="PPT_AUTO_VBA_W2PPT"></el-option>
-                              <el-option key="CONTENT_SERVER_NODE" label="CONTENT_SERVER_NODE" value="CONTENT_SERVER_NODE"></el-option>
-                            </el-select>
-                            <br><br>
-                            <span style="margin-right: 20px;font-size: 15px;">数量</span>  
-                            <el-select style="width: 250px;" v-model="dynamicExpansion.num" placeholder="请选择">
-                              <el-option v-for="(item,index) in 50"
-                                          :key="index"
-                                          :label="item"
-                                          :value="item"
-                                          >
-                              </el-option>
-                            </el-select>
-                            <br><br>
-                            <span style="margin-right: 20px;font-size: 15px;">时间</span>  
-                              <el-input-number v-model="dynamicExpansion.time" style="width:250px;" @change="handleChange" :min="1" :max="3" label="描述文字"></el-input-number>
-                              <span style="font-size: 13px; color:gray; position: relative; left: 10px; top:5px">小时</span>
-                            <br><br><br>
-                            <el-button  size="mini" style="width:295px;margin-left: 5px;" type="primary" >开始扩容</el-button>
-                            <br><br>
-                            <!-- <img v-if="fileList[0].url" :src="fileList[0].url" style="width: 100px;height: 100px;" >
-                            <i v-if="fileList[0].url" style="font-size: 16px;margin-left: 10px;cursor: pointer;" class="el-icon-delete" 
-                                    @click="deleteImg"></i>
-                            <div v-if="fileList.length ===0 " style="margin-top: 10px;" >无封面</div>
-                            <el-upload
-                              
-                            multiple 
-                            class="upload-demo"
-                                      action="https://jsonplaceholder.typicode.com/posts/"
-                                      :on-preview="handlePreview"
-                                      :on-remove="handleRemove"
-                                      :before-remove="beforeRemove"
-                                      :limit="3"
-                                      :on-exceed="handleExceed"
-                                      :file-list="fileList">
-                                      <el-button size="small" type="primary">点击上传</el-button>
-                                      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-                                    </el-upload>
-                            <br><br> -->
-                        </div>
-                    </div>
-                  </div>
+          <div style="display: flex ;align-items: center; justify-content: center;">
+              <CP_MyForm @getShow="getShow"/>
+          </div>
+        </div>
     </CP_MyDialog>
     
   </div>
@@ -74,6 +31,7 @@ import TestSass from './TestSass.vue'
 import TextBasket from './TextBasket.vue';
 import CP_table_fakePaging from './CP_table_fakePaging.vue';
 import CP_MyDialog from './CP_MyDialog.vue'
+import CP_MyForm from './CP_MyForm.vue'
 
 export default {
   name: 'HelloWorld',
@@ -85,6 +43,7 @@ export default {
     TextBasket,
     CP_table_fakePaging,
     CP_MyDialog,
+    CP_MyForm
   },
   data(){
     return{
@@ -190,43 +149,15 @@ export default {
         "id": 11
     },
       ],
-      /* FUNCTION: 表单弹窗 - 相关 */
+      /* FUNCTION: 弹窗 - 相关 */
       isShows: false,
-      dynamicExpansion:{
-        type:'PPT_AUTO_VBA_J2PPT',
-        time: '1',
-        num: '1'
-      },
-      expanding:false,
-      // 上传相关
-      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
-      
     }
   },
   methods:{
     // 获取关闭弹窗变量
     getShow(val){
       this.isShows = val
-    },
-    // 计数器
-    handleChange(val){
-      console.log(val);
-    },
-
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
-    },
-    handlePreview(file) {
-      console.log(file);
-    },
-    handleExceed(files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-    },
-    beforeRemove(file, fileList) {
-      console.log(fileList);
-      return this.$confirm(`确定移除 ${ file.name }？`);
     }
-    
   }
 }
 </script>
