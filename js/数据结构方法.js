c04b6bc9f97794c73fff719a90d739943575b48a
Myzhibo <douzhibo111@163.com>
2023-09-22 15:40:29 +0800

    数组方法

--- js/数据结构方法.js
+++ js/数据结构方法.js
@@ -2,24 +2,31 @@ console.log('**************  字符串  **************')
 // replace
 str = '1234TOS0S'
 newStr = str.replace('T', ' ')
-console.log(newStr)
+console.log("'replace'---", newStr)
 
 console.log('**************  数组  **************')
 // splice
 arr = [1, 2, 3, 4, 5]
 i = arr.indexOf(2)
 arr.splice(i, 1)
-console.log('splice---', arr)
+console.log("'splice'---", arr)
 
 // filter
 arr = [1, 2, 3, 4, 5]
 newArr = arr.filter((item) => {
   return item != 2
 })
-console.log('filter---', newArr)
+console.log("'filter'---", newArr)
 
 arr = [{ _id: 1 }, { _id: 2 }, { _id: 3 }, { _id: 4 }, { _id: 5 }]
 newArr = arr.filter((item) => {
   return item._id != 2
 })
-console.log('filter2---', newArr)
+console.log("'filter2'---", newArr)
+
+// map
+arr = [1, 2, 3, 4, 5]
+newArr = arr.map((item) => {
+  return item + 1
+})
+console.log("'map'---", newArr)
--- test_vue/README.md
+++ test_vue/README.md
@@ -15,8 +15,8 @@
 
 # 笔记
 
-1. 关于 App.vue 、'<router-view />'、router 之间的关系
+1. 关于 App.vue 、'router-view'、router 之间的关系
 
-- App.vue 在模版中定义<router-view /> 用来指向当前组件下的路由展示区
-- router/index.js 中 component: Layout 主路由指向的组件 就是 App.vue 中<router-view />所指向的画面
-- 到了 Layout/index.vue 中模版仍然定义了<router-view />， 那他就指向了路由中 component: Layout 所重定向的 path 所指向的 component: HelloWorld
+- App.vue 在模版中定义 router-view 用来指向当前组件下的路由展示区
+- router/index.js 中 component: Layout 主路由指向的组件 就是 App.vue 中 router-view 所指向的画面
+- 到了 Layout/index.vue 中模版仍然定义了 router-view ， 那他就指向了路由中 component: Layout 所重定向的 path 所指向的 component: HelloWorld
