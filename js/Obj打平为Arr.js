87b0a8ba59eaf26355ed6434302c4d85fdcbc2b6
Myzhibo <douzhibo111@163.com>
2023-10-10 11:55:00 +0800

    feat: Obj打平为Arr

---
+++ js/Obj打平为Arr.js
@@ -0,0 +1,53 @@
+let MyObj = [
+  {
+    id: 1,
+    content: '1第一层',
+    children: [
+      {
+        id: 2,
+        content: '1第二层',
+        children: [
+          {
+            id: 3,
+            content: '1级第三层',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    content: '2第一层',
+    children: [
+      {
+        id: 5,
+        content: '2第二层',
+        children: [],
+      },
+    ],
+  },
+  {
+    id: 6,
+    content: '3第一层',
+    children: [],
+  },
+]
+
+const flatData = (obj, parentId = undefined) => {
+  return obj.reduce((prev, curr) => {
+    if (parentId !== undefined) {
+      Object.assign(curr, {
+        parentId: parentId,
+      })
+    }
+    prev.push(curr)
+    if (curr.children.length > 0) {
+      prev.push(...flatData(curr.children, curr.id))
+    }
+    return prev
+  }, [])
+}
+
+let flated = flatData(MyObj)
+console.log(flated)
