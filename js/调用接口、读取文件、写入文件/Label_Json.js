2d78cb8b308361aec9d7b3bb792dde3c90ab6b72
Myzhibo <douzhibo111@163.com>
2023-12-11 11:05:56 +0800

    feat: 更新-调用接口读取文件写入文件脚本

--- js/调用接口、读取文件、写入文件/Label_Json.js
+++ js/调用接口、读取文件、写入文件/Label_Json.js
@@ -14,6 +14,9 @@ const rl = readline.createInterface({
 var express = require('express') //express框架模块
 var fs = require('fs') //文件模块
 var bodyParser = require('body-parser') //对post请求的请求体进行解析模块
+const { checkPrime } = require('crypto')
+const { resolve } = require('path')
+const path = require('path')
 var app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -38,7 +41,7 @@ function myGetSubjectAndStage(ticket_id) {
       data: {},
       headers: {
         cookie:
-          'UBUS=YnbsvrY-EBt2vCx3Em-leD4KllM-smqhtkCHNhNvTcoDJR3WwyqQ1TEHfRyrOGYW',
+          '__wpkreporterwid_=5590064d-ff16-4ea5-12b9-0deac887465f; Hm_lvt_99c7c33e9929a415fd774d2698b152e1=1698647865,1700276548; UBUS=eZfbFx8mBwZYI7jsH7mHdXqJtKWzQbKAToHHOAZQCjt0TpBPuP6X4vZyPx9oLkTA', // 若验证错误：Login invalid identity  ==>> 需要更新cookie
       },
     })
       .then((response) => {
@@ -66,7 +69,7 @@ function myGetDownJSON(ticket_id, jsonId) {
       data: {},
       headers: {
         cookie:
-          'UBUS=YnbsvrY-EBt2vCx3Em-leD4KllM-smqhtkCHNhNvTcoDJR3WwyqQ1TEHfRyrOGYW',
+          '__wpkreporterwid_=5590064d-ff16-4ea5-12b9-0deac887465f; Hm_lvt_99c7c33e9929a415fd774d2698b152e1=1698647865,1700276548; UBUS=eZfbFx8mBwZYI7jsH7mHdXqJtKWzQbKAToHHOAZQCjt0TpBPuP6X4vZyPx9oLkTA',
       },
     })
       .then((response) => {
@@ -108,6 +111,27 @@ function myGet(project_id) {
       .then((response) => {
         const data = response.data
         resolve(data)
+      })
+      .catch((error) => {
+        console.error('请求失败:', error)
+      })
+  })
+}
+
+// GET请求 获取学科学段
+function GetSubStage() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`http://worksheet.hexinedu.com/api/open/rel/v1/subject_stage`)
+      .then((response) => {
+        const data = response.data.data
+        let subInfo = []
+        data.study_section_list.forEach((i) => {
+          i.grade_subject_list[0].subject_list.forEach((s) => subInfo.push(s))
+        })
+        subInfo.push(data.civil_section_list[0].stage_section)
+        subInfo.push(data.civil_section_list[0].subject_section)
+        resolve(subInfo)
       })
       .catch((error) => {
         console.error('请求失败:', error)
@@ -174,10 +198,61 @@ function generate(ticket_id, subject, stage, filename, timer) {
   })
 }
 
+// 检查处理完的json是否存在prob字段
+async function check() {
+  fs.readdir('./result', (err, files) => {  
+    if (err) {  
+      console.error('Error reading directory:', err);  
+      return;  
+    }
+    files.forEach(file => {  
+      // 排除掉文件夹  
+      const fullPath = path.join('./result', file);  
+      const stats = fs.statSync(fullPath);  
+      if (stats.isFile()) {  
+        // 打印文件路径  
+        // console.log(fullPath);  
+        // 读取文件内容
+        const content = fs.readFileSync(fullPath, 'utf8');  
+        let flated = flatData(JSON.parse(content))
+        for (let i in flated) {
+          for (let j in flated[i].tag) {
+            // console.log(flated[i].tag)
+            if (Object.values(flated[i].tag).find(obj => obj.hasOwnProperty('prob'))) {
+              console.log(fullPath, ' -> 成功 <- ' );
+              return true
+            } else {
+              console.log(fullPath, '打标签失败！' , false);
+              return false
+            }
+          }
+        }
+      }  
+    });  
+  });
+}
+
+// 打平
+const flatData = (obj, parentId = undefined) => {
+  return obj.reduce((prev, curr) => {
+    if (parentId !== undefined) {
+      Object.assign(curr, {
+        parentId: parentId,
+      })
+    }
+    prev.push(curr)
+    if (curr.children?.length > 0) {
+      prev.push(...flatData(curr.children, curr.id))
+    }
+    return prev
+  }, [])
+}
+
 async function main() {
-  // 输入工单号
+  // // 输入工单号
   const uInput = await input()
   const ticket_ids = uInput?.match(/[a-zA-Z\d]{24}/g) ?? []
+  console.log(ticket_ids)
   for (let ticket_id of ticket_ids) {
     const { subject, stage, business_project_id, book_name } =
       await myGetSubjectAndStage(ticket_id)
@@ -200,29 +275,59 @@ async function main() {
     let timer = {}
     await generate(ticket_id, subject, stage, book_name, timer)
   }
-
-  /** 直接对目录中的json进行打码*/
-  // // 获取当前目录下除package以外全部的json文件
-  // let jsonFiles = []
-  // fs.readdir(__dirname, async (err, files) => {
-  //   if (err) {
-  //     console.error(err)
-  //     return
-  //   }
-  //   jsonFiles = files.filter((file) => {
-  //     return (
-  //       file.split('.')[file.split('.').length - 1] === 'json' &&
-  //       !file.split('.')[file.split('.').length - 2].includes('package')
-  //     )
-  //   })
-  //   let timer = {}
-  //   for (let file of jsonFiles) {
-  //     fileArr = file.split('.')
-  //     fileArr.pop()
-  //     filename = fileArr.join('.')
-  //     console.log(jsonFiles)
-  //     await generate(subject, stage, filename, timer)
-  //   }
-  // })
+  
+    // 最终check 生成物中是否有'prob'标签
+    check()
 }
-main()
+
+async function main2() {
+  /** 直接对目录中的json进行打码*/
+  // 获取当前目录下除package以外全部的json文件
+  let jsonFiles = []
+  fs.readdir(__dirname, async (err, files) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    jsonFiles = files.filter((file) => {
+      return (
+        file.split('.')[file.split('.').length - 1] === 'json' &&
+        !file.split('.')[file.split('.').length - 2].includes('package')
+      )
+    })
+    let timer = {}
+    console.log(jsonFiles)
+    for (let file of jsonFiles) {
+      fileArr = file.split('.')
+      fileArr.pop()
+      filename = fileArr.join('.')
+      console.log(jsonFiles)
+      // 获取学科、学段英文
+      let subject = ''
+      let stage = filename.includes('小学')
+        ? 'primary'
+        : filename.includes('初中')
+        ? 'junior'
+        : filename.includes('高中')
+        ? 'senior'
+        : ''
+      const subInfo = await GetSubStage()
+      for (let i in subInfo) {
+        if (filename.includes(subInfo[i].cn_name)) {
+          subject = subInfo[i].en_name
+        }
+      }
+      await generate('--', subject, stage, filename, timer)
+    }
+  })
+
+  // 最终check 生成物中是否有prob标签
+  check()
+}
+
+// 通过工单号 先生成原生json再打码，生成最终生成物json
+// main()
+check()
+
+// 直接对目录中原生json打码，生成最终json
+// main2()
