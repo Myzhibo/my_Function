1a98a5b3bc536347389309387edfe209a8e0cfdc
Myzhibo <douzhibo111@163.com>
2023-12-26 18:14:45 +0800

    feat: arr合并同名字段

---
+++ js/数据结构方法/arr合并同名字段.js
@@ -0,0 +1,35 @@
+json = [
+  {
+    id: 1,
+    result: 'a'
+  },
+  {
+    id: 1,
+    result: 'b'
+  },
+  {
+    id: 2,
+    result: 'c'
+  },
+  {
+    id: 3,
+    result: 'd'
+  },
+  {
+    id: 3,
+    result: 'e'
+  }
+]
+
+
+const transformedJson = json.reduce((acc, item) => {  
+  const existingItem = acc.find(i => i.id === item.id);  
+  if (existingItem) {  
+    existingItem.result.push(item.result);  
+  } else {  
+    acc.push({ ...item, result: [item.result] });  
+  }  
+  return acc;  
+}, []);  
+  
+console.log(transformedJson);
