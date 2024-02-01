aa9c7762ce6007b45ae1f8575f989574f66380b4
Myzhibo <douzhibo111@163.com>
2024-02-01 09:54:59 +0800

    feat: 监听事件

---
+++ vue_methods/监听事件.js
@@ -0,0 +1,19 @@
+
+mounted(){
+  // keydown 监听键盘事件
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      console.log('Esc key pressed!');
+    }
+  });
+
+  //  scroll 监听全局滚动事件
+  document.addEventListener('scroll', function (event) {
+    console.log('滚动了');
+  });
+  // 监听特定的div滚动事件
+  const scrollableDiv = document.getElementById('scrollableDivId');
+  scrollableDiv.addEventListener('scroll', function (event) {
+    console.log('Esc key pressed!');
+  });
+},
